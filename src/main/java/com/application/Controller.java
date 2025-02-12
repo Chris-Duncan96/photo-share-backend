436f3e66c32bf47da4507fb8224e6d21ddeb5ca1
Chris-Duncan96 <chris.duncan96@gmail.com>
2025-02-12 12:46:48 -0600

    controller test

--- src/main/java/com/application/Controller.java
+++ src/main/java/com/application/Controller.java
@@ -1,11 +1,17 @@
 package com.application;
     
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
     
 @RestController
 public class Controller {
     Service service = new Service();
+
+    @GetMapping("/")
+    public String index() {
+        return "ok";
+    }
 
     @GetMapping("/health")
     public String health() {
@@ -13,7 +19,12 @@ public class Controller {
     }
 
     @GetMapping("/photos/{photoId}")
-    public String getPhoto(String photoId) {
+    public String getPhoto(@PathVariable String photoId) {
         return service.getPhoto(photoId);
+    }
+
+    @GetMapping("/test")
+    public String getService() {
+        return service.getExample();
     }
 }
--- src/main/java/com/application/Service.java
+++ src/main/java/com/application/Service.java
@@ -4,4 +4,8 @@ public class Service {
     public String getPhoto(String photoId) {
         return "Photo " + photoId;
     }
+
+    public String getExample() {
+        return "Example";
+    }
 }
