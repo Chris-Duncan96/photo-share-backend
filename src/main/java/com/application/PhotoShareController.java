bace668cedb28197ebbf72ec95f5df90958e2899
Chris-Duncan96 <chris.duncan96@gmail.com>
2025-02-12 17:15:42 -0600

    fully remove mentions of cors

--- src/main/java/com/application/PhotoShareController.java
+++ src/main/java/com/application/PhotoShareController.java
@@ -1,14 +1,12 @@
 package com.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "*")
 public class PhotoShareController {
 
     @Autowired
